278c7f7308aeacef166ee22dd4530c119f4a514b
unknown <contemateo6@gmail.com>
2025-11-06 05:13:09 -0300

    Agrego páginas y base de med y perfumes

--- index.js
+++ index.js
@@ -18,4 +18,27 @@ overlay.addEventListener("click", () => {
   menu.classList.add("translate-x-full");
   overlay.classList.add("opacity-0", "pointer-events-none");
 });
+const drawer = document.getElementById("drawer");
+const drawerPanel = document.getElementById("drawerPanel");
+const closeDrawer = document.getElementById("closeDrawer");
+const continueShopping = document.getElementById("continueShopping");
+
+document.querySelector('[data-commandfor="drawer"]').addEventListener("click", () => {
+  drawer.classList.remove("hidden");
+  setTimeout(() => {
+    drawerPanel.classList.remove("translate-x-full");
+  }, 10);
+});
+
+function closeCart() {
+  drawerPanel.classList.add("translate-x-full");
+  setTimeout(() => drawer.classList.add("hidden"), 300);
+}
+
+closeDrawer.addEventListener("click", closeCart);
+continueShopping.addEventListener("click", closeCart);
+drawer.addEventListener("click", e => {
+  if (e.target === drawer) closeCart();
+});
+
 
